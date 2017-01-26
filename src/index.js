d25a54fe8fa7a5de0adc8a9a5729d44cb945dd99
Tomohiro SHIOYA <shioyang4321@gmail.com>
2017-01-26 23:35:44 +0900

    Change to redux style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import reducers from './reducers';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
+const store = createStore(reducers);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+const cvApp = combineReducers({
+});
+
+export default cvApp;
